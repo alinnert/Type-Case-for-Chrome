0a1bc3568e21f36b4fb4a275e7aeba7c83d0c8d7
Andreas Linnert <andreas@linnertmedia.de>
2016-08-11 22:58:01 +0200

    v0.5.2 - [FIX] files couldn't be opened anymore due to an Chrome API change - [NEW] added 3 new themes: Material Blue, Material Tael, Material Green - [CHG] app uses the fonts "Open Sans" and "Source Code Pro" now - [CHG] changed the settings dialog layout a little

--- background.js
+++ background.js
@@ -8,25 +8,25 @@
 chrome.app.runtime.onLaunched.addListener(onLaunchedListener);
 
 function onLaunchedListener(launchData) {
-    const options = {
-        id: 'mainWindow',
-        outerBounds: {
-            width: 900,
-            height: 750
-        },
-        frame: {
-            color: '#4b4b4b',
-            inactiveColor: '#6b6b6b'
-        }
-    };
-
-    chrome.app.window.create('index.html', options, createWindowCallback);
-
-    function createWindowCallback(createdWindow) {
-        createdWindow.contentWindow.launchData = launchData;
-
-        if (createdWindow.contentWindow.checkLaunchData instanceof createdWindow.contentWindow.Function) {
-            createdWindow.contentWindow.checkLaunchData();
-        }
+  const options = {
+    id: 'mainWindow',
+    outerBounds: {
+      width: 900,
+      height: 750
+    },
+    frame: {
+      color: '#4b4b4b',
+      inactiveColor: '#6b6b6b'
     }
+  };
+
+  chrome.app.window.create('index.html', options, createWindowCallback);
+
+  function createWindowCallback(createdWindow) {
+    createdWindow.contentWindow.launchData = launchData;
+
+    if (createdWindow.contentWindow.checkLaunchData instanceof createdWindow.contentWindow.Function) {
+      createdWindow.contentWindow.checkLaunchData();
+    }
+  }
 }
